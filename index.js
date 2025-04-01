2a1e722757c68d5d1b26b3d2fd874559310e4521
Mingyung Park <m11ngyung3@gmail.com>
2025-04-01 11:09:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ const parser = new Parser({
 
 <br/>
 
-**❗Machine Learning**  
+**❗웹사이트 핑거프린팅 공격 모델 프로젝트**  
 ***[JARVIS Repository](https://github.com/ZERO-black/2023-2ML-Team-JARVIS)***  
 
     Website Fingerprinting Attack on Tor Network 
